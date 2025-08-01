71913d0e6a654f1195bac8365a98f3fe0f53c11c
Nick Xiong <kou20.xiong30@gmail.com>
2025-08-01 16:31:03 -0400

    2nd commit changes in VS2022

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -5,5 +5,9 @@
         public string Email { get; set; } = string.Empty;
 
         public string Address { get; set; } = string.Empty;
+
+        public string City { get; set; } = string.Empty;
+
+        public string State { get; set; } = string.Empty;
     }
 }
